eeba185083b92ee6ed8815da2aff1aa6421e5191
isamarietr <isa.torres@gmail.com>
2021-05-04 13:24:37 -0400

    Use single defaultReducer

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./reducers";
+// import rootReducer from "./reducers";
+import defaultReducer from "./reducers/defaultReducer"
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(rootReducer, composeWithDevTools(
+export const store = createStore(defaultReducer, composeWithDevTools(
   applyMiddleware(thunk),
   // other store enhancers if any
 ));
